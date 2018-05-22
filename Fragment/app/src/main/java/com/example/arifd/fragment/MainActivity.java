dfc5fc8e577f12aa6291691572b6b8e8dbfa3477
Arif Romdon <arifdop182@gmail.com>
2018-05-22 12:03:45 +0700

    Fragment with Asynctask

--- Fragment/app/src/main/java/com/example/arifd/fragment/MainActivity.java
+++ Fragment/app/src/main/java/com/example/arifd/fragment/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.arifd.fragment;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private EditText time;
+    private TextView finalResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        time = findViewById(R.id.inTime);
+        button = findViewById(R.id.buttonRun);
+        finalResult = findViewById(R.id.textResult);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTaskRunner runner = new AsyncTaskRunner();
+                String sleepTime = time.getText().toString();
+                runner.execute(sleepTime);
+            }
+        });
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 //            inisialisasi nilai fragmentManager untuk berinteraksi dengan activity saat ini
@@ -26,5 +48,43 @@ public class MainActivity extends AppCompatActivity {
 //            mengeksekusi fragment transaction
 
 
+    }
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+        private String resp;
+        ProgressDialog progressDialog;
+
+        @Override
+        protected String doInBackground(String...params){
+            publishProgress("Sleeping...");
+            try {
+                int time = Integer.parseInt(params[0])*1000;
+                Thread.sleep(time);
+                resp = "Sleep for "+params[0]+" seconds";
+            }catch (InterruptedException e){
+                e.printStackTrace();
+                resp = e.getMessage();
+            }catch (Exception e){
+                e.printStackTrace();
+                resp = e.getMessage();
+            }
+            return resp;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            progressDialog.dismiss();
+            finalResult.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute(){
+            progressDialog = ProgressDialog.show(MainActivity.this, "ProgressDialog",
+                    "Wait for "+time.getText().toString()+" seconds");
+        }
+
+        @Override
+        protected void onProgressUpdate(String... text){
+            finalResult.setText(text[0]);
+        }
     }
 }
