945eff1443524b1b4fb51c8bf3205b1b8e69652f
Arif Romdon <arifdop182@gmail.com>
2018-06-04 14:51:03 +0700

    Retrofit Mocky

---
+++ retro/app/src/main/java/com/example/arifd/retro/userAPI.java
@@ -0,0 +1,30 @@
+package com.example.arifd.retro;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+
+//retrofit
+
+public interface userAPI {
+
+    @GET("/v2/59b753140f00008e0e7126b4")
+    Call<Users> getUsers();
+
+    @GET("/api/v1/auth{id}")
+    Call<User> getUser(@Path("id") String user_id);
+
+    @PUT("/api/v1/auth/{id}")
+    Call<User> updateUser(@Path("id") int user_id, @Body User user);
+
+    @POST("/api/v1/auth")
+    Call<User> saveUser(@Body User user);
+
+    @DELETE("/api/v1/auth/{id}")
+    Call<User> deleteUser(@Path("id") String user_id);
+}
